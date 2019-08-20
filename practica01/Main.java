7808ca089bcc356d62e69323bda1299e0f890e76
Manuel-V13 <manuel_mtz_peralta@ciencias.unam.mx>
2019-08-20 10:47:27 -0500

    PRACTICA1

--- practica01/Main.java
+++ practica01/Main.java
@@ -1,9 +1,8 @@
-	public class Main{
-		public static void main(String[]args){
+public class Main{
+	public static void main(String[]args){
 		System.out.println("Hola");
 		System.out.println("java 11.0.2 2019-01-15 LTS");			
 		System.out.println("Java(TM) SE Runtime Environment 18.9 (build 11.0.2+9-LTS)");
 		System.out.println("Java HotSpot(TM) 64-Bit Server VM 18.9 (build 11.0.2+9-LTS, mixed mode)");
-
-		}
+	}
 }
